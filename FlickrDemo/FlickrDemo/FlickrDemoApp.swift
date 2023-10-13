dae2bb5dcd1c00a0e550bf53162079eb45686456
Ben Walker <benwalker92@me.com>
2023-10-13 09:45:18 +0100

    Create Xcode project

---
+++ FlickrDemo/FlickrDemo/FlickrDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrDemoApp.swift
+//  FlickrDemo
+//
+//  Created by Ben Walker on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlickrDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
